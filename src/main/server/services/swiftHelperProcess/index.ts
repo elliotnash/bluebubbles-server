ac6145c8d223ef4c62e4d040cedb31a7f375e0a3
Elliot <elliotnash@gmail.com>
2021-11-17 15:52:50 -0800

    use sock path from application support

--- src/main/server/services/swiftHelperProcess/index.ts
+++ src/main/server/services/swiftHelperProcess/index.ts
@@ -1,52 +1,56 @@
 import { Server } from "@server/index";
-import * as net from "net"
-import * as fs from "fs-extra"
+import * as net from "net";
+import * as fs from "fs-extra";
 import { ChildProcess, spawn } from "child_process";
+import { app } from "electron";
 import SocketMessage from "./socketMessage";
 
 export class SwiftHelperService {
+    sockPath: string;
 
-  server: net.Server;
+    server: net.Server;
 
-  helper: net.Socket;
+    helper: net.Socket;
 
-  child: ChildProcess;
+    child: ChildProcess;
 
-  startServer() {
-    fs.removeSync("testsock.sock");
-    this.server = net.createServer((client) => {
-      Server().log("Swift Helper connected");
-      client.on('end', () => {
-        Server().log("Swift Helper disconnected");
-      });
-      client.on('data', (data) => {
-        const msg = SocketMessage.fromBytes(data);
-      })
-      this.helper = client;
-    });
-    this.server.listen('testsock.sock');
-  }
+    startServer() {
+        fs.removeSync(this.sockPath);
+        this.server = net.createServer(client => {
+            Server().log("Swift Helper connected");
+            client.on("end", () => {
+                Server().log("Swift Helper disconnected");
+            });
+            client.on("data", data => {
+                const msg = SocketMessage.fromBytes(data);
+            });
+            this.helper = client;
+        });
+        this.server.listen(this.sockPath);
+    }
 
-  runSwiftHelper() {
-    Server().log("Starting Swift Helper");
-    this.child = spawn('swiftHelper/.build/release/swiftHelper');
-    this.child.stdout.setEncoding('utf8');
-    this.child.stdout.on('data', (data) => {
-      Server().log(`Swift Helper: ${data.toString().trim()}`);
-    });
-    this.child.stderr.setEncoding('utf8');
-    this.child.stderr.on('data', (data) => {
-      console.log(`Swift Helper error: ${data}`);
-    });
-    this.child.on('close', (code) => {
-      this.runSwiftHelper();
-    });
-  }
+    runSwiftHelper() {
+        Server().log("Starting Swift Helper");
+        this.child = spawn("swiftHelper/.build/release/swiftHelper", [this.sockPath]);
+        this.child.stdout.setEncoding("utf8");
+        this.child.stdout.on("data", data => {
+            Server().log(`Swift Helper: ${data.toString().trim()}`);
+        });
+        this.child.stderr.setEncoding("utf8");
+        this.child.stderr.on("data", data => {
+            Server().log(`Swift Helper error: ${data}`);
+        });
+        this.child.on("close", code => {
+            Server().log("Swift Helper process exited");
+            this.runSwiftHelper();
+        });
+    }
 
-  start() {
-    // Configure & start the socket server
-    Server().log("Starting Private API Helper...", "debug");
-    this.startServer();
-    setTimeout(this.runSwiftHelper, 100);
-  }
+    start() {
+        this.sockPath = `${app.getPath("userData")}/swift-helper.sock`;
+        // Configure & start the socket server
+        Server().log("Starting Private API Helper...", "debug");
+        this.startServer();
+        setTimeout(this.runSwiftHelper, 100);
+    }
 }
